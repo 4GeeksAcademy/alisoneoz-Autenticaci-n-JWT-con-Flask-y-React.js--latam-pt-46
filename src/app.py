15297d5c3e1a11946f7107598bc53bcdaa16b0e2
Alison Estephany <96319139+Alisoneoz@users.noreply.github.com>
2025-05-07 20:58:03 +0000

    initial commit

--- src/app.py
+++ src/app.py
@@ -6,10 +6,14 @@ from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
-from api.models import db
+from api.models import db, User
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from flask_bcrypt import Bcrypt
+
+
+
 
 # from models import Person
 
@@ -18,7 +22,7 @@ static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-
+bcrypt = Bcrypt(app)
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
@@ -65,6 +69,30 @@ def serve_any_other_file(path):
     response.cache_control.max_age = 0  # avoid cache memory
     return response
 
+@app.route('/signup', methods=['POST'])
+def add_new_user():
+    body = request.get_json()
+    if not body:
+        return jsonify({
+            "message":"Body is required"
+        }),400
+    if "email" not in body:
+          return jsonify({
+            "message":"email is required"
+        }),400
+    if "password" not in body:
+        return jsonify({
+            "message":"password is required"
+        }),400   
+    email=body["email"]
+    password = body.get("password")
+    is_active = body.get("is_active", True)
+    hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
+
+    user= User(email=email, password=hashed_password, is_active=is_active)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(user.serialize()),201
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
