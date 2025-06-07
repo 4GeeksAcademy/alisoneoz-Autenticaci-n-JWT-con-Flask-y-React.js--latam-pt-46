5b0e9e22a45fd7c65a852b9346ce248348d45582
Alison Estephany <96319139+Alisoneoz@users.noreply.github.com>
2025-06-07 03:13:34 +0000

    improve register IU

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from "react"
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 import { createUser } from "../services/createUser.js";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
+
+	const navigate = useNavigate()
 
 	const { store, dispatch } = useGlobalReducer()
 
 	const [usuarioNuevo, setUsuarioNuevo] = useState({
-		email:"",
-		password:""
+		email: "",
+		password: ""
 	})
 
-	const handleCrearUsuario = async (e)=> {
+	const handleCrearUsuario = async (e) => {
 		e.preventDefault();
-		try{
+		try {
 			await createUser(usuarioNuevo)
-		} catch(error){
+			navigate("/login")
+		} catch (error) {
 			console.log("hubo un error al crear el User:", error)
 		}
 	}
@@ -47,29 +51,30 @@ export const Home = () => {
 	}, [])
 
 	return (
-		<div className="text-center mt-5 bg-light content-fluid p-5">
-			<form className=""
-			onSubmit={handleCrearUsuario}
+		<div className="mt-5 bg-light content-fluid p-5 d-flex justify-content-center flex-column align-items-center gap-5">
+			<h1>Welcome, please register!</h1>
+			<form className="w-75 text-start gap-5"
+				onSubmit={handleCrearUsuario}
 			>
-			<label>Email</label>
-					<input					
-						type="text" className="form-control" placeholder="youremail@companyofyouremail.com" aria-label="Username" aria-describedby="basic-addon1" 
-						onChange={(e) => {							
-							console.log(e.target.value)
-							setUsuarioNuevo({ ...usuarioNuevo, email: e.target.value });
-						  }}
-						value={usuarioNuevo.email}
-					/>
-					<label>password</label>
-				
-					<input type="text" className="form-control" placeholder="password" aria-label="Username" aria-describedby="basic-addon1" 
-						onChange={(e) => {
-							setUsuarioNuevo({ ...usuarioNuevo, password: e.target.value }) 
-							console.log(e.target.value)
-						}}
-						value={usuarioNuevo.password}
-					/>
-		<button className="btn btn-success">Send</button>
+				<label>Email</label>
+				<input
+					type="text" className="form-control mt-2" placeholder="youremail@companyofyouremail.com" aria-label="Username" aria-describedby="basic-addon1"
+					onChange={(e) => {
+						console.log(e.target.value)
+						setUsuarioNuevo({ ...usuarioNuevo, email: e.target.value });
+					}}
+					value={usuarioNuevo.email}
+				/>
+				<label>password</label>
+
+				<input type="text" className="form-control mt-2" placeholder="password" aria-label="Username" aria-describedby="basic-addon1"
+					onChange={(e) => {
+						setUsuarioNuevo({ ...usuarioNuevo, password: e.target.value })
+						console.log(e.target.value)
+					}}
+					value={usuarioNuevo.password}
+				/>
+				<button className="btn btn-success mt-2 w-100">Send</button>
 			</form>
 		</div>
 	);
