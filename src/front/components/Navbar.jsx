4f0e2815af0b49f5ee75db3578ee32449f0907fc
alisoneoz <alisonestephanyoz@gmail.com>
2025-06-11 18:17:06 -0400

    add close session logic

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,20 +1,36 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import useGlobalReducer from "../hooks/useGlobalReducer";
 
 export const Navbar = () => {
+	const { store, dispatch } = useGlobalReducer()
+	const navigate = useNavigate();
+
+	const handleLogout = () => {
+		dispatch({ type: 'cerrar_la_sesion' });
+		navigate("/")
+	}
 
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1">Authentication with JWT</span>
 				</Link>
+
 				<div className="ml-auto">
-					<Link to="/login" className="mx-5">
-						Login
-					</Link>
-					<Link to="/">
-						Register
-					</Link>
+					{store.token ? (
+						<button className="btn btn-secondary me-2" onClick={handleLogout}>
+							Cerrar sesión
+						</button>
+					) : (<>
+						<Link to="/login" className="mx-5">
+							Login
+						</Link>
+						<Link to="/">
+							Register
+						</Link></>
+					)}
+
 				</div>
 			</div>
 		</nav>
